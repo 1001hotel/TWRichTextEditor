1df667712ccfe786dd2e7d88483fa77969faafde
xurenyan <812610313@qq.com>
2017-05-16 16:44:57 +0800

    commit

---
+++ TWRichTextEditorDemo/ZSSRichTextEditor/RYIndicatorBackgroudView.h
@@ -0,0 +1,15 @@
+//
+//  RYIndicatorBackgroudView.h
+//  ZSSRichTextEditor
+//
+//  Created by luomeng on 2017/5/2.
+//  Copyright © 2017年 Zed Said Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RYIndicatorBackgroudView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame withRightDistance:(float)distance;
+
+@end
